a801fa5d18ed8d011db09711cbb9daabfc6a029f
gaurav mishra <151965437+gauravmishraaa@users.noreply.github.com>
2025-11-20 02:32:53 +0530

    Delete media/media/src/main/java/com/social/media directory

--- media/media/src/main/java/com/social/media/respositories/SocialProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.social.media.respositories;
-
-import com.social.media.models.SocialProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SocialProfileRepository extends JpaRepository<SocialProfile,Long> {
-}
--- media/media/src/main/java/com/social/media/respositories/SocialUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.social.media.respositories;
-
-import com.social.media.models.SocialUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SocialUserRepository extends JpaRepository<SocialUser,Long> {
-}
